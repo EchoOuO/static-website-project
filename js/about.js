c38626bfe0112a117d5c016c76245325b24df12a
EchoOuO <window9995@gmail.com>
2024-01-30 17:29:06 -0800

    20240130
    1. fix broken html link to css
    2. working on JS

---
+++ js/about.js
@@ -0,0 +1,13 @@
+// 為啥動不了呢~~~  html 那邊也有加onclick了呀
+function toggleParagraph() {
+    const paragraph = document.querySelector('.faq-detail');
+
+    // Toggle the display property
+    if (paragraph.style.display === 'none') {
+        paragraph.style.display === 'block';
+    } else {
+        paragraph.style.display === 'none';
+    }
+};
+
+
